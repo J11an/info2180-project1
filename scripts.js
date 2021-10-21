6819a17d758331807b893dafc6342424a0d31c18
J.Wan <jaysun1w@gmail.com>
2021-10-21 11:17:33 -0500

    Javascript email function added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+
+    let btn = document.querySelector(".btn:nth-child(2)");
+
+    btn.addEventListener("click",function(event){
+        let e  = document.querySelector("#email").value;
+        let m = document.querySelector(".message");
+        const echeck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+        console.log("button clicked");
+        if (e.length != 0 && e.includes("@") && echeck.test(e)){
+
+			m.textContent = "Thank you! Your email address " + e + " has been added to our mailing list!";
+
+		}
+		else{
+            
+			m.textContent = "Please enter a valid email address";
+            
+		}
+        event.preventDefault();
+
+
+   });
+}
+
